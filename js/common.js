60b6eee18070062c9e355dcbcf695307fe90812e
Danilo Pagano <dmpeletronic@gmail.com>
2016-04-20 01:24:09 -0300

    Criada a funcao boxProduto() e aplicada em todos os htmls. Adicionados arquivos do netbeans.

--- js/common.js
+++ js/common.js
@@ -3,5 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var boxProduto = function(elements) {
+    var div = "<div class='produto'>";
+    var _div = "</div>";
+    var link = "<a href="+elements.CaminhoImagem+" target=blank>"; 
+    var _link = "</a>";
+    var img = "<img src='"+ elements.CaminhoImagem +
+            "' width='320' height='240' alt='"+elements.Codigo+
+            "' title='"+elements.Codigo+
+            " Saldo:"+elements.SaldoEstoque+
+            " Preco:"+elements.PrecoVenda+"'>";
+    var html = div + link + img + _link + _div;
+    return html;
+}
 
